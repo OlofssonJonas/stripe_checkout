c26d5d672b37505f8f2d83cdc31a911af9ee5a53
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-09-18 09:23:49 +0200

    Order in json

--- client/src/components/Confirmation/Confirmation.jsx
+++ client/src/components/Confirmation/Confirmation.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineCoffee } from 'react-icons/ai'
 
 const Confirmation = () => {
+  const [ isPaymentVeried, setIsPaymentVerified ] = useState(false)
+
+  useEffect(() => {
+    const sessionId = localStorage.getItem("session-id")
+    const verifyPayment = async() => {
+      const response = await fetch(
+        "/api/verify-session",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({sessionId}),
+        }
+        );
+        const { verified } = await response.json()
+
+        if(verified) {
+          setIsPaymentVerified(true)
+          localStorage.removeItem("session-id")
+        } else {
+          setIsPaymentVerified(false)
+        }
+
+    } 
+    verifyPayment()
+  }, [])
+
   return (
-    <h1>Tack för ditt köp, ta en kopp <AiOutlineCoffee /> och invänta din beställning</h1>
+    isPaymentVeried ? <h1>Tack för ditt köp, ta en kopp <AiOutlineCoffee /> och invänta din beställning</h1> : <h1>Nåt gick fel</h1>
   )
 }
 
--- server/Controllers/orders.controller.js
+++ server/Controllers/orders.controller.js
@@ -1,10 +1,46 @@
 
 const initStripe = require ("../stripe")
 const stripe = initStripe()
+const path = require('path')
+const fs = require('fs')
+const filePath = path.join(__dirname, "..", "data", "orders.json")
 
 const verifyPayment = async(req, res) => {
-    console.log(req.body.sessionId)
-    res.status(200).json('Det här gick ju bra:) ')
+    try{
+        const session = await stripe.checkout.sessions.retrieve(req.body.sessionId);
 
-}
-module.exports = { verifyPayment }
+        
+        if(session.payment_status !== "paid") {
+            return res.status(400).json({verified: false})
+        }
+        
+        const line_items = await stripe.checkout.sessions.listLineItems(req.body.sessionId)
+        
+
+            const order = {
+                created: session.created,
+                customer: session.customer_details.name,
+                products: line_items.data.map(item => {
+                    return {
+                        product: item.description,
+                        price: item.price.unit_amount / 100,
+                        quantity: item.quantity,
+                    }
+                }) 
+               
+            };
+            let orders = []
+            // const fileData = fs.writeFileSync(filePath, 'utf8')
+            // orders = JSON.parse(fileData)
+
+            orders.push(order)
+
+            fs.writeFileSync(filePath, JSON.stringify(orders, null, 2))
+
+            res.status(200).json({ verified: true})
+            
+        } catch (error){
+            console.log(error)
+        }
+    }
+        module.exports = { verifyPayment }
